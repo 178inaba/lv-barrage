89a12a29c075a8481812aa6bb61607a2f6b06002
178inaba <178inaba@users.noreply.github.com>
2017-06-13 12:58:29 +0900

    Add signal handling

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+)
+
 func main() {
+	log.SetFlags(0)
+	log.SetPrefix(fmt.Sprintf("%s: ", os.Args[0]))
+	os.Exit(run())
+}
+
+func run() int {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+	_, cancel := context.WithCancel(context.Background())
+	go func() {
+		<-sigCh
+		cancel()
+	}()
+
+	return 0
 }
