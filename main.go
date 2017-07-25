fedd3e734e07b95e91b4b3a32216d1d8170aa25a
178inaba <178inaba@users.noreply.github.com>
2017-07-25 23:35:59 +0900

    Fix usage

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func main() {
 func run() int {
 	args := flag.Args()
 	if len(args) != 2 {
-		fmt.Fprintf(os.Stderr, "usage: %s [-a] [-c=<comment_color>] [-o] <live_id or live_url> <comment>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "usage: %s [-a] [-c <comment_color>] [-o] <live_id | live_url> <comment>\n", os.Args[0])
 		flag.PrintDefaults()
 		return 1
 	}
